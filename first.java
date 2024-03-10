365abc709b48c492339274cd6ed04e108f77db91
Alisha <alisha0141.be21@chitkara.edu.in>
2024-03-10 13:02:23 +0530

    my first program

---
+++ first.java
@@ -0,0 +1,12 @@
+package javaprograms;
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println(s);
+        //abcd
+    }
+    
+}
